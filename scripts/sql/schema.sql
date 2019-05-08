779cc2bccff9ae55b04e456e7adece4850dfe485
Denis Zinovyev <zinoviev.denis@yandex.ru>
2019-05-08 16:24:29 +0300

    refactoring

--- scripts/sql/schema.sql
+++ scripts/sql/schema.sql
@@ -1,14 +1,14 @@
-CREATE TABLE "document"(
-    id TEXT
-        CONSTRAINT documents_id_pk PRIMARY KEY,
-    title TEXT
-        NOT NULL,
+CREATE TABLE document(
+                         id TEXT
+                             CONSTRAINT documents_id_pk PRIMARY KEY,
+                         title TEXT
+                             NOT NULL,
 
-    owner_id TEXT
-        NOT NULL,
+                         owner_id TEXT
+                             NOT NULL,
 
-    subject TEXT
-        NOT NULL,
+                         subject TEXT
+                             NOT NULL,
 
-    CONSTRAINT documents_title_owner_id_subject_unique UNIQUE(title, owner_id, subject)
+                         CONSTRAINT documents_title_owner_id_subject_unique UNIQUE(title, owner_id, subject)
 );
